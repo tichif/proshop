89db3fcf928d876cb0bb29195b15f9c8f3950f17
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-15 22:23:54 -0600

    Create Product and Rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
-  return <>
-  <Header />
-    <main className="py-3">
-      <Container>
-      <h1>Welcome to Proshop</h1>
-      </Container>
-    </main>
-    <Footer />
-  </>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
