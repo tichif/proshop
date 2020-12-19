38f5b972045e1fdb9640a3be5557becced9ef5c0
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-19 09:17:05 -0600

    Connect the app to the database and add colors to the terminal

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+
+import productsRoutes from './routes/products.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
-import productsRoutes from './routes/products.js';
-
 const app = express();
+
+connectDB();
 
 app.use('/api/products', productsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running in ${process.env.NODE_ENV} on port: ${PORT}`);
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port: ${PORT}`.yellow.bold
+  );
 });
