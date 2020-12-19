df2cfea5f222e413f7db9606d786a8463ce69a4d
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-18 23:28:25 -0600

    Fetch the products from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,64 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import axios from 'axios';
 
 import Rating from '../components/Rating';
-import products from '../products';
-
 const ProductScreen = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, []);
 
   return (
     <>
-      <Link className=" btn btn-light my-3" to="/">Go back</Link>
+      <Link className=' btn btn-light my-3' to='/'>
+        Go back
+      </Link>
       <Row>
-        <Col md="6">
+        <Col md='6'>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
-        <Col md="3">
-          <ListGroup variant="flush">
+        <Col md='3'>
+          <ListGroup variant='flush'>
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} ${
-              product.numReviews > 1 ? 'reviews' : 'review'
-            }`}></Rating>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} ${
+                  product.numReviews > 1 ? 'reviews' : 'review'
+                }`}
+              ></Rating>
             </ListGroup.Item>
-            <ListGroup.Item>
-              Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Description: {product.description}
-            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md="3">
+        <Col md='3'>
           <Card>
-            <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  Price
-                </Col>
-                <Col>
-                  <strong>${product.price}</strong>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  Status
-                </Col>
-                <Col>
-                  {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add to cart</Button>
-            </ListGroup.Item>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
