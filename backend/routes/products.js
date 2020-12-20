cd4edf986fee0fff053328b80d9aa5dc411eeb93
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-19 21:05:31 -0600

    Change the products array to Product model

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,16 +1,34 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
-import products from '../data/products.js';
+import Product from '../models/Product.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// @desc    Fetch single products
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ msg: 'product not found' });
+    }
+  })
+);
 
 export default router;
