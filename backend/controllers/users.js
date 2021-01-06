9b44230d4a93bfa56a51fb3b17f000c94b511465
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-01-05 19:20:56 -0600

    Generate a token with jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import generateToken from '../utils/generateToken.js';
 
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -13,7 +14,7 @@ export const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
