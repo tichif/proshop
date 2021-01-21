b820a70d00639f49a647e88704febef97e6b5494
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-01-20 23:39:54 -0600

    Create PaymentScreen and save payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route exact path='/shipping' component={ShippingScreen} />
+          <Route exact path='/payment' component={PaymentScreen} />
           <Route exact path='/login' component={LoginScreen} />
           <Route exact path='/register' component={RegisterScreen} />
           <Route exact path='/profile' component={ProfileScreen} />
