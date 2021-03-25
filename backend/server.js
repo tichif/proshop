c6396c65f9e8adedc28b3895a4c7cafa1ef62cc4
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-03-25 19:24:55 -0400

    Create the upload file endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -5,6 +7,7 @@ import colors from 'colors';
 import productsRoutes from './routes/products.js';
 import usersRoutes from './routes/users.js';
 import ordersRoutes from './routes/orders.js';
+import uploadsRoutes from './routes/uploads.js';
 import connectDB from './config/db.js';
 import { error, notFound } from './middleware/error.js';
 
@@ -19,10 +22,15 @@ app.use(express.json());
 app.use('/api/products', productsRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/orders', ordersRoutes);
+app.use('/api/uploads', uploadsRoutes);
 
 app.use('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+// Make static folder
+const __dirname = path.resolve(); // only for module syntax in ES6
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
